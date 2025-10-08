dde991fa579243714a07cd5f953dd7049cd25ac6
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-10-08 17:37:01 +0300

    create the model class for the pet

---
+++ app/src/main/java/com/vtol/petpal/domain/model/Pet.kt
@@ -0,0 +1,11 @@
+package com.vtol.petpal.domain.model
+
+data class Pet(
+    val id: Int,
+    val petName: String,
+    val imagePath: String,
+    val birthDate: Long?,
+    val gender: PetGender,
+    val specie: String?,
+    val weight: Double,
+)
---
+++ app/src/main/java/com/vtol/petpal/domain/model/PetGender.kt
@@ -0,0 +1,5 @@
+package com.vtol.petpal.domain.model
+
+enum class PetGender {
+    MALE, FEMALE
+}
