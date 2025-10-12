9c1b6e17f1cc5da72ee57fe8a090a3df8396f3c9
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-10-12 06:19:36 +0300

    add 'gender' & 'birthdate' fields in pet add screen

--- app/src/main/java/com/vtol/petpal/presentation/pets/components/PetTextField.kt
+++ app/src/main/java/com/vtol/petpal/presentation/pets/components/PetTextField.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -13,17 +14,37 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.vtol.petpal.domain.model.WeightUnit
 import com.vtol.petpal.ui.theme.PetPalTheme
 
 @Composable
-fun PetTextField(modifier: Modifier = Modifier,keyboardOptions: KeyboardOptions?= null,placeHolder: String,value: String,leadingIcon: ImageVector? = null,onValueChanged: (String) -> Unit){
+fun PetTextField(
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions? = null,
+    placeHolder: String,
+    value: String,
+    selectedUnit: WeightUnit? = null,
+    trailingIcon: ImageVector? = null,
+    onTrailingClicked: (() -> Unit)? = null,
+    onValueChanged: (String) -> Unit,
+) {
     OutlinedTextField(
-        modifier = modifier.fillMaxWidth().padding(bottom = 12.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 12.dp),
         value = value,
         colors = OutlinedTextFieldDefaults.colors(unfocusedBorderColor = Color.LightGray),
         onValueChange = { onValueChanged(it) },
-        placeholder = { Text(text=placeHolder, color = Color.LightGray) },
+        placeholder = { Text(text = placeHolder, color = Color.LightGray) },
         shape = RoundedCornerShape(10.dp),
+        trailingIcon = {
+            trailingIcon?.let {
+                IconButton(onClick = {}) {
+                    Text(selectedUnit?.name?.lowercase() ?: "")
+                }
+            }
+
+        },
         keyboardOptions = keyboardOptions ?: KeyboardOptions.Default
     )
 
@@ -31,7 +52,7 @@ fun PetTextField(modifier: Modifier = Modifier,keyboardOptions: KeyboardOptions?
 
 @Preview
 @Composable
-fun MyPreview(){
+fun MyPreview() {
     PetPalTheme {
         PetTextField(placeHolder = "Pet Name", value = "", onValueChanged = {})
     }
