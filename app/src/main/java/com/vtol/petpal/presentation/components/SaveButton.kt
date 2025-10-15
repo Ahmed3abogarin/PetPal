f4b00d1079dba5d98371ab0f69abc359b9be1cd9
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-10-15 23:51:22 +0300

    create common button component

---
+++ app/src/main/java/com/vtol/petpal/presentation/components/SaveButton.kt
@@ -0,0 +1,29 @@
+package com.vtol.petpal.presentation.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SaveButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color,
+    onClick: () -> Unit,
+) {
+    Button(
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0XFFFFBC5E)),
+        onClick = { onClick() }) {
+        Text(text = "Save Feed", fontSize = 20.sp)
+    }
+}
