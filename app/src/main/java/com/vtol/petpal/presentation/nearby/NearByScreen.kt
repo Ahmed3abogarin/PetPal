e55bd2366bbcca64309466b5485a67f3a75726cf
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-10-30 21:47:58 +0300

    create the list for the near vets

--- app/src/main/java/com/vtol/petpal/presentation/nearby/NearByScreen.kt
+++ app/src/main/java/com/vtol/petpal/presentation/nearby/NearByScreen.kt
@@ -3,35 +3,43 @@ package com.vtol.petpal.presentation.nearby
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Application
-import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.vtol.petpal.presentation.nearby.components.LocationList
 import com.vtol.petpal.ui.theme.PetPalTheme
+import kotlinx.coroutines.launch
 
 
 @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
@@ -40,7 +48,6 @@ fun NearByScreenContent() {
     val application = LocalContext.current.applicationContext as Application
 
     val cameraPositionState = rememberCameraPositionState()
-
 
 
     val viewModel: NearViewModel = viewModel(
@@ -62,58 +69,51 @@ fun NearByScreenContent() {
         }
     }
 
-//    val context = LocalContext.current
-//    var hasLocationPermission by remember { mutableStateOf(false) }
-//    var currentLatLng by remember { mutableStateOf(LatLng(0.0, 0.0)) }
-//
-//    val permissionLauncher = rememberLauncherForActivityResult(
-//        contract = ActivityResultContracts.RequestPermission()
-//    ) { granted ->
-//        hasLocationPermission = granted
-//        if (granted) getCurrentLocation(context) { lat, lng ->
-//            currentLatLng = LatLng(lat, lng)
-//        }
-//    }
-
-//    LaunchedEffect(Unit) {
-//        permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-//    }
-//
-//    val cameraPositionState = rememberCameraPositionState {
-//        position = CameraPosition.fromLatLngZoom(currentLatLng, 15f)
-//    }
-//
-//    val markerState = remember { MarkerState(position = currentLatLng) }
+    val coroutineScope = rememberCoroutineScope()
 
 
+    var selectedVet by remember { mutableIntStateOf(0) }
 
+    val pagerState = rememberPagerState(initialPage = selectedVet) {
+        vets.size
+    }
 
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-    ) {
-
-        vets.forEach {
-            Log.v("gjkdsghs","ITs: ${it.address}")
-            Marker(state = MarkerState(LatLng(it.lat,it.lng)),)
+    LaunchedEffect(pagerState.currentPage) {
+        if (vets.isNotEmpty()) {
+            val vet = vets[pagerState.currentPage]
+            cameraPositionState.animate(
+                update = CameraUpdateFactory.newLatLngZoom(LatLng(vet.lat, vet.lng), 13f),
+                durationMs = 500
+            )
         }
-//        if (hasLocationPermission) {
-//            Marker(state = markerState, title = "You are here")
-//        }
+    }
 
 
+    Box(modifier = Modifier.fillMaxSize()) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            vets.forEachIndexed { index, vet ->
+                Log.v("gjkdsghs", "ITs: ${vet.address}")
+                Marker(state = MarkerState(LatLng(vet.lat, vet.lng)), onClick = {
+                    selectedVet = index
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                    true
+                })
+            }
+        }
+        LocationList(
+            Modifier
+                .align(Alignment.BottomCenter),
+            vets,
+            pagerState
+        )
     }
 }
 
-@SuppressLint("MissingPermission")
-private fun getCurrentLocation(context: Context,onLocation: (Double, Double) -> Unit) {
-    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
-    fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-        if (location != null) {
-            onLocation(location.latitude, location.longitude)
-        }
-    }
-}
 
 @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
 @Composable
@@ -121,7 +121,6 @@ fun NearByScreen() {
     val context = LocalContext.current
 
     var hasLocationPermission by remember { mutableStateOf(false) }
-    var userLocation by remember { mutableStateOf<LatLng?>(null) }
 
     // Permission launcher
     val permissionLauncher = rememberLauncherForActivityResult(
---
+++ app/src/main/java/com/vtol/petpal/presentation/nearby/components/LocationList.kt
@@ -0,0 +1,110 @@
+package com.vtol.petpal.presentation.nearby.components
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.vtol.petpal.R
+import com.vtol.petpal.domain.model.Vet
+import com.vtol.petpal.ui.theme.MainPurple
+
+
+@Composable
+fun LocationList(modifier: Modifier,locationList: List<Vet>, pagerState: PagerState) {
+
+    HorizontalPager(
+        modifier = modifier,
+        state = pagerState,
+        contentPadding = PaddingValues(horizontal = 20.dp)
+    ) {
+        LocationCard(locationList[it])
+    }
+
+}
+
+@Composable
+fun LocationCard(vet: Vet) {
+    val context = LocalContext.current
+    Card(
+        modifier = Modifier
+            .height(230.dp)
+            .width(320.dp),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.elevatedCardElevation(12.dp),
+        onClick = {
+            Toast.makeText(context,vet.name, Toast.LENGTH_SHORT).show()
+        }
+    ) {
+        Column {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.65f),
+                contentScale = ContentScale.FillWidth,
+                painter = painterResource(R.drawable.location_img),
+                contentDescription = "location image"
+            )
+
+            Text(
+                modifier = Modifier.padding(start = 10.dp, top = 8.dp),
+                fontWeight = FontWeight.Bold,
+                text = vet.name
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .padding(horizontal = 10.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "open util 9:00 AM", color = MainPurple)
+
+                Row {
+                    Icon(Icons.Default.Star, tint = Color.Yellow, contentDescription = "")
+                    Text(text = "5.5", color = Color.Black)
+
+                }
+
+            }
+        }
+
+
+    }
+
+}
+
+//@Preview
+//@Composable
+//fun LocationListPreview() {
+//    val list = listOf(
+//        Vet("", 4.00, 6.00, "")
+//    )
+//    PetPalTheme {
+//        LocationList(Modifier,list)
+//    }
+//}
