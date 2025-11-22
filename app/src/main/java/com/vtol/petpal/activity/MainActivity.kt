2a1a4c97902ea183fe84d921e0008b5dcdaa3ffd
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-11-22 22:39:42 +0300

    organize the files

--- app/src/main/java/com/vtol/petpal/activity/MainActivity.kt
+++ app/src/main/java/com/vtol/petpal/activity/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import com.vtol.petpal.presentation.navgraph.NavGraph
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PetPalTheme {
-                Box(modifier = Modifier.Companion.fillMaxSize()) {
+                Column(modifier = Modifier.Companion.fillMaxSize()) {
                     NavGraph()
                 }
             }
