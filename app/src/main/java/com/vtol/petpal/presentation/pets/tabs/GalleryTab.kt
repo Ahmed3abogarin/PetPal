a103c9be56ad5c684cc584b9fe98ed1910190f1b
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-12-31 20:48:07 +0300

    create free plan screen for GalleryTab.kt in Pet details screen

--- app/src/main/java/com/vtol/petpal/presentation/pets/tabs/GalleryTab.kt
+++ app/src/main/java/com/vtol/petpal/presentation/pets/tabs/GalleryTab.kt
@@ -1,28 +1,77 @@
 package com.vtol.petpal.presentation.pets.tabs
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.FilledIconButton
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.StarBorder
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vtol.petpal.ui.theme.DarkGold
+import com.vtol.petpal.ui.theme.Gold
+import com.vtol.petpal.ui.theme.PetPalTheme
 
 @Composable
 fun GalleryTab(modifier: Modifier = Modifier, isPremium: Boolean, onUpgradeClicked: () -> Unit) {
 
-    if (isPremium){
+    if (isPremium) {
 
 
-    }else{
-        Box(modifier= Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = "Upgrade to premium to access the gallery")
-            FilledIconButton(onClick = { onUpgradeClicked() }) {
-                Text(text = "Upgrade to Premium")
+    } else {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "Upgrade to premium to access the gallery")
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    shape = RoundedCornerShape(16.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Gold),
+                    onClick = { onUpgradeClicked() }
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(text = "Upgrade", style = MaterialTheme.typography.titleMedium)
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Surface(
+                            shape = CircleShape,
+                            color = DarkGold,
+                            border = BorderStroke(width = 2.dp, color = Color.White)
+                        ) {
+                            Icon(
+                                modifier = Modifier.size(32.dp).padding(6.dp),
+                                imageVector = Icons.Default.StarBorder,
+                                contentDescription = "icon"
+                            )
+                        }
+                    }
+                }
             }
         }
     }
+}
 
+@Preview
+@Composable
+fun GalleryPreview() {
+    PetPalTheme {
+        GalleryTab(isPremium = false, onUpgradeClicked = {})
 
-
+    }
 }
