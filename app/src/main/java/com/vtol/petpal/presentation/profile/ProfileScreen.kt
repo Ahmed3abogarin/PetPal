3f2d511157d5f1cd8cec2cf47e5ca3829f4bc91d
Ahmed <ahmedadilabogarin@gmail.com>
2026-01-15 21:15:30 +0300

    change the image place holder in profile screen

--- app/src/main/java/com/vtol/petpal/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/vtol/petpal/presentation/profile/ProfileScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -16,12 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.vtol.petpal.R
 import com.vtol.petpal.presentation.profile.components.SettingsButton
 import com.vtol.petpal.ui.theme.BackgroundColor
@@ -33,6 +32,7 @@ fun ProfileScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(BackgroundColor)
+            .padding(top = 6.dp)
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -40,16 +40,17 @@ fun ProfileScreen() {
         Text(text = "My Profile", style = MaterialTheme.typography.headlineLarge)
         Spacer(modifier = Modifier.height(24.dp))
 
+        // Todo : Add Async image
         Image(
             modifier = Modifier
                 .size(140.dp)
                 .clip(CircleShape),
-            painter = painterResource(R.drawable.profile_img),
+            painter = painterResource(R.drawable.profile_ph),
             contentScale = ContentScale.Crop,
             contentDescription = "profile image"
         )
-        Spacer(modifier = Modifier.height(12.dp))
-        Text(text = "Ahmed Adil", fontSize = 20.sp, color = Color.Gray)
+//        Spacer(modifier = Modifier.height(12.dp))
+//        Text(text = "Ahmed Adil", fontSize = 20.sp, color = Color.Gray)
         Spacer(modifier = Modifier.height(32.dp))
 
         SettingsButton(
