0fbbed0216d1abc535af19bbeb2550bf6abd4fa0
Ahmed <ahmedadilabogarin@gmail.com>
2026-04-23 22:42:20 +0300

    bump version code to 6 and version name to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.vtol.petpal"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
